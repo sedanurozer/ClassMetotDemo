7619987c492458a3199ad37600455e0719160753
asfalothsed <63976703+asfalothsed@users.noreply.github.com>
2021-02-08 21:15:22 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+
+    {
+     public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + musteri.Adi + musteri.Soyadi +"Bilgilerine sahip müşteri sisteme başarılı bir şekilde eklenmiştir.");
+        }
+        public void Listele(Musteri musteri1)
+        {
+            Console.WriteLine("Müşteri Id= " + musteri1.Id);
+            Console.WriteLine("Müşteri Adı= " + musteri1.Adi);
+            Console.WriteLine("Müşteri Soyadı= " + musteri1.Soyadi);
+            Console.WriteLine("Müşteri bilgileri listelenmiştir.....");
+        }
+
+
+     public void Delete(Musteri musteri1)
+        {
+            Console.WriteLine(musteri1.Id + musteri1.Adi  + musteri1.Soyadi + "Bilgilerine sahip müşteri sistemden başarılı bir şekilde silinmiştir.");
+            
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Adi = "Sedanur";
+            musteri.Soyadi = "Özer";
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Add(musteri);
+            musterimanager.Listele(musteri);
+            musterimanager.Delete(musteri);
+         
+
+        }
+    }
+}
